d74af4ee31f68e6e8cd53d5fd3ba8aeda1d8bc0d
Zerro2003 <mugishaelie160@gmail.com>
2025-10-06 20:35:00 +0200

    adds space for music list

--- index.php
+++ index.php
@@ -6,9 +6,10 @@
     <link rel="stylesheet" href="output.css" />
   </head>
   <body>
+    
     <form
       class="flex flex-row gap-8 h-20 items-center justify-center mt-30 mx-20 border-2 rounded-3xl"
-      action="Post.php"
+      action="./"
       method="POST"
     >
       <input
@@ -40,7 +41,24 @@
       </button>
     </form>
     <div
-      class="flex flex-row gap-8 h-20 items-center justify-center mx-20 border-2 rounded-3xl"
+      class="flex flex-row gap-8 h-20 items-center justify-center mx-20 border-2 rounded-3xl mt-8"
     ></div>
+    <?php
+include 'connect.php';
+$title = isset($_POST['title']) ? $_POST['title'] : '';
+$artist = isset($_POST['artist']) ? $_POST['artist'] : '';
+$duration = isset($_POST['duration']) ? $_POST['duration'] : '';
+
+
+$sql = "INSERT INTO songs (title, artist, duration) VALUES ('$title', '$artist', '$duration')";
+
+// if (mysqli_query($conn, $sql)) {
+//     echo "✅ Song added successfully!";
+// } else {
+//     echo "❌ Error: " . mysqli_error($conn);
+// }
+
+mysqli_close($conn);
+?>
   </body>
 </html>
